5c52b8177d6067fc0e598eb7e98560f1d0cddce0
ricarlos <ricarlosgp@gmail.com>
2023-01-05 13:35:24 -0300

    utilizando o POST

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const app = express();
+                const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hello, World!")
-})
+                app.post("/users", (request, response) => {
+
+                    response.send("Você chamou o POST");
+                });
 
 const PORT = 3333; 
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); 
